e07a0d269426eead1e506b7c5f9209959400e075
Adarshktk <137869114+Adarshktk@users.noreply.github.com>
2024-02-07 15:47:13 +0530

    Add files via upload

---
+++ project/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'project'
+
+urlpatterns = [
+    path('', views.projects, name='projects'),
+    path('add/', views.add, name='add'),
+    path('<uuid:pk>/',views.project, name='project'),
+    path('<uuid:pk>/edit/', views.edit, name='edit'),
+    path('<uuid:pk>/delete/', views.delete, name='delete'),
+    path('<uuid:project_id>/upload_file/', views.upload_file, name = 'upload_file'),
+    path('<uuid:project_id>/files/<uuid:pk>/delete/', views.delete_file, name='delete_file'),
+    path('<uuid:project_id>/add_note/', views.create_note, name='add_note'),
+    path('<uuid:project_id>/<uuid:pk>/note_detail/', views.note_detail, name='note_detail'),
+    path('<uuid:project_id>/<uuid:pk>/note_edit/', views.note_edit, name='note_edit'),
+    path('<uuid:project_id>/<uuid:pk>/note_delete/', views.note_delete, name='note_delete'),
+]
